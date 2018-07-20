c4d2dbaf6d924c5e35f77cd6d0cb31e226013e8e
Tomasz Martyniak <tom4r@users.noreply.github.com>
2018-07-20 19:03:19 +0200

    Added ZMQ blocks

---
+++ python/pwsat2_frame_source.py
@@ -0,0 +1,45 @@
+import numpy
+from gnuradio import gr
+import collections
+import pmt
+import array
+import zmq
+import thread
+
+
+class pwsat2_frame_source(gr.basic_block):
+    """
+    docstring for block hdlc_framer
+    """
+    def __init__(self, address, isBound):
+        gr.basic_block.__init__(self,
+            name="pwsat2_frame_source",
+            in_sig=None,
+            out_sig=None)
+
+        self.address = address
+        self.message_port_register_out(pmt.intern('out'))
+
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        print "Address: |%s|" % address
+        if isBound:
+            self.socket.bind(address)
+        else:
+            self.socket.connect(address)
+            
+        self.socket.setsockopt(zmq.SUBSCRIBE, "")
+
+        thread.start_new_thread(self.loop, ())
+
+    def loop(self):
+        while True:
+            if self.socket.poll(timeout=1) > 0:
+                buff = self.socket.recv()
+                buff = [ord(i) for i in buff]
+
+                print buff
+
+                pkt = pmt.cons(pmt.PMT_NIL, pmt.init_u8vector(len(buff), buff))
+
+                self.message_port_pub(pmt.intern('out'), pkt)
